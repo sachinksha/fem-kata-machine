119ae81e415ade7ae663ee2c1c06002fb2cd2d11
Sachin Kiran Shah <sachinksha@gmail.com>
2026-04-21 22:30:32 +0530

    added code for two crystal balls problem and bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,1 +1,17 @@
-export default function bubble_sort(arr: number[]): void {}
+export default function bubble_sort(arr: number[]): void {
+    // for (let i = 0; i < arr.length; i++) {
+    //     for (let j = 0; j < arr.length - 1 - i; j++) {
+    //         if (arr[j] > arr[j+1]) {
+    //             [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+    //         }
+    //     }
+    // }
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i; j < arr.length; j++) {
+            if (arr[i] > arr[j]) {
+                [arr[i], arr[j]] = [arr[j], arr[i]];
+            }
+        }
+    }
+}
