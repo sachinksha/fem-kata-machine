6de83f2252f60f5ea3dbf3cfd7f6f11136957291
Sachin Kiran Shah <sachinksha@gmail.com>
2026-05-30 23:05:40 +0530

    Added code for implementing doubly linked list and debug settings

--- src/day1/DoublyLinkedList.ts
+++ src/day1/DoublyLinkedList.ts
@@ -1,12 +1,128 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
+    private head?: Node<T>;
+    private tail?:Node<T>;
 
-    constructor() {}
+    constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
+    }
 
-    prepend(item: T): void {}
-    insertAt(item: T, idx: number): void {}
-    append(item: T): void {}
-    remove(item: T): T | undefined {}
-    get(idx: number): T | undefined {}
-    removeAt(idx: number): T | undefined {}
+    prepend(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
+        if(!this.head){
+            this.head = this.tail = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+
+    insertAt(item: T, idx: number): void {
+        if(idx >this.length){
+            throw new Error("oh no");
+        }
+        else if(idx ===this.length){
+            this.append(item);
+            return;
+        }
+        else if(idx === 0) {
+            this.prepend(item);
+            return;
+        }
+        this.length++;
+        let current = this.head;
+        let currentIndex = 0;
+        while(current && currentIndex != idx){
+            currentIndex++;
+            current = current.next;
+        }
+        current = current as Node<T>;
+        const node = {value:item} as Node<T>;
+        node.next = current;
+        node.prev = current.prev;
+        if (current.prev) {
+            current.prev.next = node;
+        }
+        current.prev = node;
+    }
+    
+    append(item: T): void {
+        this.length++;
+        const node = {value:item} as Node<T>;
+        if(!this.tail){
+            this.head = this.tail = node;
+            return;
+        }
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node; 
+    }
+
+    remove(item: T): T | undefined {
+        let current = this.head;
+        while(current != null && current.value !== item){
+            current = current.next;
+        }
+        if(current){
+            return this.removeNode(current);
+        }
+        return undefined;
+    }
+
+    get(idx: number): T | undefined {
+        return this.getAt(idx)?.value;
+    }
+
+    removeAt(idx: number): T | undefined {
+        const curr = this.getAt(idx);
+        if(curr){
+            return this.removeNode(curr);
+        }
+        return undefined;
+    }
+
+    private getAt(idx:number): Node<T> | undefined {
+        if(idx > this.length) {
+            throw new Error ("oh no");
+        }
+        let curr = this.head;
+        for(let i=0; i < this.length && curr !== null ; i++) {
+            if(i === idx) {
+                return curr;
+            }
+            curr = curr?.next;
+        }
+        return undefined;
+    }
+    private removeNode(node: Node<T>): T {
+        this.length--;
+        if(this.length === 0) {
+            this.head = this.tail = undefined;
+            return node.value;
+        }
+        if(node.prev){
+            node.prev.next = node.next;
+        }
+        if(node.next){
+            node.next.prev = node.prev;
+        }
+        if(node === this.head){
+            this.head = node.next;
+        }
+        if(node === this.tail){
+            this.tail = node.prev;
+        }
+        node.next = node.prev = undefined;
+        return node.value;
+    }
 }
