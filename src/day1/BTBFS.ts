b9390735072cbeef4bd20858c6d17195a7baa68d
Sachin Kiran Shah <sachinksha@gmail.com>
2026-05-31 23:54:55 +0530

    added code for breadth first search and comparison of binary search trees

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,4 +1,19 @@
-export default function bfs(
-    head: BinaryNode<number>,
-    needle: number,
-): boolean {}
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q:(BinaryNode<number> | null | undefined) [] = [head];
+    while(q.length) {
+        const current = q.shift() as BinaryNode<number> | undefined | null;
+        if(!current){
+            continue;
+        }
+        if(current.value === needle) {
+            return true;
+        }
+        if(current.left){
+            q.push(current.left);
+        }
+        if(current.right) {
+            q.push(current.right);
+        }
+    }
+    return false;
+}
