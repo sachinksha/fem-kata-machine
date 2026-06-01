c5765c1635f5a3aba18a02c931477dc12fba6986
Sachin Kiran Shah <sachinksha@gmail.com>
2026-06-01 13:04:41 +0530

    added code to implement DFS on BST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,4 +1,20 @@
+function search(node:BinaryNode<number> | null, needle:number):boolean {
+    if(!node) {
+        return false;
+    }
+    if(node.value === needle){
+        return true;
+    }
+    else if(node.value < needle) {
+        return search(node.right, needle);
+    }
+    else {
+        return search(node.left, needle);
+    }
+}
 export default function dfs(
     head: BinaryNode<number>,
     needle: number,
-): boolean {}
+): boolean {
+    return search(head, needle);
+}
