7b72b428ca77eed659db767e89fc244bc434770e
Sachin Kiran Shah <sachinksha@gmail.com>
2026-06-06 22:10:27 +0530

    added code to implement LRU

--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,8 +1,96 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+    prev?: Node<T>,
+}
+
+function createNode<V>(value:V):Node<V> {
+    return {value}
+}
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
 
-    constructor() {}
+    private lookup: Map<K, Node<V>>;
+    private reverseLookup: Map<Node<V>, K>;
 
-    update(key: K, value: V): void {}
-    get(key: K): V | undefined {}
+
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head  = this.tail = undefined;
+        this.lookup = new Map<K, Node<V>>();
+        this.reverseLookup = new Map<Node<V>, K>();
+    }
+
+    update(key: K, value: V): void {
+        let node = this.lookup.get(key);
+        if(!node){
+            node = createNode(value);
+            this.length++;
+            this.prepend(node);
+            this.trimCache();
+            this.lookup.set(key, node);
+            this.reverseLookup.set(node, key);
+        } else {
+            this.detach(node);
+            this.prepend(node);
+            node.value = value;
+        }
+        // does it exist?
+        // get()?
+        // if it doesnt, we need to insert
+
+        //  -- check capacity and evict if over
+        // if it does, we need to update to the font of the list
+        // and update the value
+    }
+    get(key: K): V | undefined {
+        // check the cache for existance
+        const node = this.lookup.get(key);
+        if(!node){
+            return undefined;
+        }
+        // update the value we found and move it to the front / head
+        this.detach(node);
+        this.prepend(node);
+        // return the value found or undefined if it does not exist
+        return node.value;
+    }
+
+    private detach(node: Node<V>):void {
+        if(node.prev) {
+            node.prev.next = node.next;
+        }
+        if(node.next) {
+            node.next.prev = node.prev;
+        }
+        if(this.head === node) {
+            this.head = this.head.next;
+        }
+        if(this.tail === node) {
+            this.tail = node.prev;
+        }
+        node.next = node.prev = undefined;
+    }
+    private prepend(node:Node<V>):void {
+        if(!this.head) {
+            this.head = this.tail = node;
+            return;
+        }
+        node.next = this.head;
+        this.head.prev = node;
+        this.head = node;
+    }
+    private trimCache(): void {
+        if(this.length <= this.capacity) {
+            return;
+        }
+        const tail = this.tail as Node<V>;
+        this.detach(this.tail as Node<V>);
+        const key = this.reverseLookup.get(tail) as K;
+        this.lookup.delete(key);
+        this.reverseLookup.delete(tail);
+        this.length--;
+    }
 }
